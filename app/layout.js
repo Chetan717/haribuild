736890f7f8e78e88bf6d1e4042d98925e0148d95
hari <harischetan717@gmail.com>
2023-09-04 17:48:55 +0530

    update now 4sep

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
     setUserData(user);
-  }, [userData]);
+  }, []);
   return (
     <html lang="en">
       <body className={`${inter.className}  `}>
